2711c74576b2c4dcd297b14da0e216b26f7efae5
Manuel Lara <manuellara@Manuels-MacBook-Air.local>
2017-09-08 20:59:06 -0700

    new

---
+++ circleClass.cpp
@@ -0,0 +1,39 @@
+/*
+
+Manuel Lara
+
+Review Question 2
+
+*/
+#include <iostream>
+
+using namespace std;
+
+const double pi = 3.14159;
+
+////circle class 
+class Circle
+{
+    private:
+        double radius;
+
+    public:
+        /////default constructor
+        Circle();
+        /////constructor 
+        Circle(double rad);
+
+        ////set & get functions
+        void setRadius(double rad);
+        void getRadius();
+        double getArea();
+        double getDiameter();
+        double getCircumfrence();
+        
+
+};
+
+int main()
+{
+    ;
+}
