327982967181ed598cfdf663f5a9d18527595dc5
ManuelLara <manuellaraa@gmail.com>
2017-11-11 23:59:16 -0800

    update

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -7,10 +7,13 @@
 
 using namespace std;
 
-////prototype
+////prototypes
 bool fileInput( ifstream &fin );
+void printStatement( string line );
 void fileAnalysis( ifstream &fin );
-void filter( string line , stack<char> &stack , vector<string> &vector );
+void filter( string line , stack<char> &stack );
+void transfer( stack<char> &stack , vector<char> &vector );
+void matching( vector<char> &vector );
 
 int main()
 {
@@ -19,6 +22,10 @@ int main()
 
     bool target = fileInput( fin );       //returns file if valid 
 
+    if ( target == false )
+    {
+        exit(0);                         //exit program if file doesnt exist
+    }
 }
 
 bool fileInput( ifstream &fin )
@@ -43,36 +50,36 @@ bool fileInput( ifstream &fin )
         return true;
     }
 }
+void printStatement( string line )
+{
+    cout << line << endl ;
+}
 
 void fileAnalysis( ifstream &fin )
 {
     string line;
     stack<char> stack;                              //stack of char
-    vector<string> vector;                            //vector declaration
-
+    vector<char> vector;                            //vector declaration
 
     while( !fin.eof() )                             //while not end of file
     {
         getline( fin , line );                      //gets line from file
+
+        //printStatement( line );                     //prints line 
         
-        filter( line , stack , vector );                       //characters are pushed onto stack 
+        filter( line , stack );                     //characters are pushed onto stack 
     }
 
     ////stack populated  end->beginning
-    ////vector populated ( index ) beginning->end
+    
+    transfer( stack , vector );                     //puts stack into vector 
 
-    cout << stack.size() << endl;
+    /////vector populated   end->beginning 
 
-    while( !stack.empty() )
-    {
-        cout << stack.top();
-        stack.pop();
-    }
-
-    cout << endl;
+    matching( vector );
 }
 
-void filter( string line , stack<char> &stack , vector<string>& vector )
+void filter( string line , stack<char> &stack )
 {    
     char whiteList[] = { '\'' , '\"' , '{' , '}' , '*' , '/' };
 
@@ -82,8 +89,62 @@ void filter( string line , stack<char> &stack , vector<string>& vector )
         {
             if( !isspace( line[i] ) && line[i] == whiteList[x] )      //if character is not white space AND is in the whitelist
             {
-                stack.push( line[i] );              //adds to stack
+                stack.push( line[i] );                                //adds to stack
             }
         }
     }
 }
+
+void transfer( stack<char> &stack , vector<char> &vector )
+{
+    while( !stack.empty() )
+    {
+        vector.push_back( stack.top() );            //transfers stack to vector 
+        stack.pop();
+    }
+}
+
+void matching( vector<char> &vector )
+{
+    char whiteList[] = { '\'' , '\"' , '{' , '}' , '*' , '/' };
+
+    int openComment = 0 , closeComment = 0 , singleQuote = 0 , doubleQuote = 0 , openBracket = 0 , closeBracket = 0;
+
+    for( int i = vector.size() ; i >= 0 ; i-- )
+    {
+        if( vector[i] == whiteList[5] && vector[i+1] == whiteList[4] ) //open comment block
+        {
+            openComment += 1 ;
+        }
+
+        if( vector[i] == whiteList[4] && vector[i+1] == whiteList[5] ) //close comment block
+        {
+            closeComment += 1 ;
+        }
+
+        if( vector[i] == whiteList[0] )  //single quote
+        {
+            singleQuote += 1 ;
+        }
+
+        if( vector[i] == whiteList[1] )  //double quote
+        {
+            doubleQuote += 1 ;
+        }
+
+        if( vector[i] == whiteList[2] )  //open bracket
+        {
+            openBracket += 1 ;
+        }
+
+        if( vector[i] == whiteList[3] )  //close bracket
+        {
+            closeBracket += 1 ;
+        }
+    }
+
+    cout << openComment << endl ;
+    cout << closeComment << endl ;
+
+}
+
