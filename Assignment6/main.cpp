f1dc17cc08aa7e726bbab68ecedefcc2dcadd76e
ManuelLara <manuellaraa@gmail.com>
2017-12-14 23:54:40 -0800

    update

--- Assignment6/main.cpp
+++ Assignment6/main.cpp
@@ -1,38 +1,107 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<cctype>
+#include<fstream>
+#include<vector>
 
-#include "wordTree.cpp"
-
-//prototypes
-void convertToLower( string &x ) ;
+#include "wordTree.h"
 
 using namespace std;
 
-int main()
+//converts string to lowercase
+void convertToLower(string &x)
 {
-    string treeData[5] = { "ECHo" , "BRAVO" , "AlPha" , "Delta" , "cHArlie" } ;
-
-    for( int i = 0 ; i < 5 ; i++ )
-    {
-        convertToLower( treeData[i] ) ;
-    }
-
-    wordTree myTree ;
-
-    for( int i = 0 ; i < 5 ; i++ )
-    {
-        myTree.add( treeData[i] ) ;
-    }
-
-    myTree.printInOrder();
-
+	for (int i = 0; i != x.length(); i++)
+	{
+		x[i] = tolower(x[i]);
+	}
 }
 
-
-void convertToLower( string &x )            //converts string to lower case
+//reads file
+bool readFile(vector<string> &v)
 {
-    for ( int i = 0 ; i < x.length()  ; i++ )
-    {
-        x[i] = tolower( x[i] ) ;
-    }
+	string word;
+
+	string target = "doi.txt";		//sample file
+	ifstream fin;					//file handler 
+
+	fin.open(target);				//apttempt to open sample file
+
+	if (fin.fail())			
+	{
+		return false;				//if fail - return false
+	}
+	else
+	{
+		while (!fin.eof())
+		{
+			fin >> word ;				//gets single word from sample file
+			v.push_back(word);
+		}
+		return true;
+	}
+}
+
+//cleans up text
+void filter(vector<string> &v)
+{
+	string word;
+
+	for (int i = 0 ; i != v.size() ; i++)				//for every word in vector
+	{
+		for (int j = 0 ; j != v[i].length() ; j++)		//for every char in word
+		{
+			if ( isalpha(v[i][j]) )
+			{
+				word += v[i][j];						//if alpha, consctuct word with char
+			}
+		}
+		v[i] = word;									//replace old word with filtered entry 
+		word = "";
+	}
+}
+
+//clean up white spaces 
+
+int main()
+{
+	vector<string> vector;
+
+	bool read = readFile(vector);			//call readFile and pass in vector
+
+	if (!read)
+	{
+		cout << "File was not read in properly..." << endl;
+
+		exit(0);
+	}
+	else
+	{
+		filter(vector);			//filter
+
+		for (int i = 0; i != vector.size(); i++)
+		{
+			convertToLower(vector[i]);
+		}
+	}
+
+	for (int i = 0 ; i != vector.size()-1 ; i++)
+	{
+		cout << vector[i] << endl;
+	}
+
+	/*wordTree myTree;
+
+	for (int i = 0 ; i != vector.size()-1 ; i++)
+	{
+		myTree.addLeaf(vector[i]);
+	}
+
+	myTree.printInOrder();
+
+	myTree.~wordTree();*/
+
+
+
+	getchar();									//pause before exiting
 }
--- Assignment6/wordTree.h
+++ Assignment6/wordTree.h
@@ -1,44 +1,132 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
+struct node
+{
+	string data;
+	node* left;
+	node* right;
+};
+
 class wordTree
 {
-    private:
-    struct wordNode
-    {
-        string data; //value
+	private:
+		node* root;
 
-        wordNode *left; //left pointer 
-        wordNode *right; //right pointer 
-    };
+		node* createLeafPrivate(string v)
+		{
+			node* n = new node;
 
-        wordNode *root; //root pointer
+			n->data = v;
+			n->right = nullptr;
+			n->left = nullptr;
 
+			return n;
+		}
 
-        void addPrivate( string v , wordNode *ptr ) ;
+		void addLeafPrivate(string v, node* ptr)
+		{
+			if (root == nullptr)
+			{
+				root = createLeaf(v);		//if root is empty, create leaf 
+			}
+			else if (v < ptr->data)			//if string less than node - go left
+			{
+				if (ptr->left != nullptr)
+				{
+					addLeafPrivate(v, ptr->left);		//if node exists, traverse down left
+				}
+				else
+				{
+					ptr->left = createLeaf(v);	//else create leaf
+				}
+			}
+			else if (v > ptr->data)				//if string greater than node - go right
+			{
+				if (ptr->right != nullptr)
+				{
+					addLeafPrivate(v, ptr->right);		//if node exists, traverse down right
+				}
+				else
+				{
+					ptr->right = createLeaf(v);	//else create leaf
+				}
+			}
+			else
+			{
+				;
+				//cout << "The word \"" << v << "\" has already been added" << endl;
+			}
+		}
 
-        void printInOrderPrivate( wordNode *ptr );
+		//IN-ORDER TRAVERSAL
+		void printInOrderPrivate(node* ptr)
+		{
+			if (root != nullptr)
+			{
+				if (ptr->left != nullptr)
+				{
+					printInOrderPrivate(ptr->left);		//go left until you reach the end
+				}
 
-    public:
-        wordTree() ;  //default constructor
+				cout << ptr->data << endl;			//proccess current node
 
+				if (ptr->right != nullptr)
+				{
+					printInOrderPrivate(ptr->right);		//go right until you reach the end
+				}
+			}
+			else
+			{
+				cout << "The tree is empty..." << endl ;
+			}
+		}
 
+	public:
+		wordTree()
+		{
+			root = nullptr;
+		}
 
-        wordNode* createLeaf( string v ); //creates leaf
+		node* createLeaf(string v)
+		{
+			return createLeafPrivate(v);
+		}
 
-        void add( string v ) ;   //adds a given word to the tree if it does not already exist & increments appropriate counter
+		void addLeaf(string v)
+		{
+			addLeafPrivate(v, root);
+		}
 
-        void printInOrder();
+		void printInOrder()
+		{
+			printInOrderPrivate(root);
+		}
 
+		//POST-ORDER TRAVERSAL
+		void removeWordTree(node* ptr)
+		{
+			if (ptr != nullptr)
+			{
+				if (ptr->left != nullptr)
+				{
+					removeWordTree(ptr->left);		//traverse all the way left
+				}
+				if (ptr->right != nullptr)
+				{
+					removeWordTree(ptr->right);		//traverse all the way right
+				}
 
-        //int distinctWords() const;  //returns # of distinct words
+				delete ptr;							//deletes node
+			}
+		}
 
-        //int totalWords() const;   //returns # of total words including duplicate values 
-
-
-
-
-        //~wordTree(); //destructor
+		~wordTree()
+		{
+			removeWordTree(root);
+			
+			cout << endl << "Tree has been deleted." << endl;
+		}
 };
